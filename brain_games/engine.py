8f46adc3eddd9d4a2890d2cc93b7e44f1c4bf56a
Khudyakov Artem <artem170289@mail.ru>
2025-08-31 17:41:25 +0300

    bug fix

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -7,20 +7,16 @@ def start_game(module):
     print(f"Hello, {user_name}!")
 
     print(module.get_condition_string(), end="\n")
-    condition_data = module.get_condition_data()
-    correct_ans = module.get_correct_answer(condition_data)
-    print(f"Question: {condition_data}")
-    answer = prompt.string("Your answer: ")
-
-    is_correct = module.is_user_correct(correct_ans, answer)
-    for _ in range(2):
+    
+    for _ in range(3):
+        condition_data = module.get_condition_data()
+        print(f"Question: {condition_data}")
+        correct_ans = module.get_correct_answer(condition_data)
+        answer = prompt.string("Your answer: ")
+        is_correct = module.is_user_correct(correct_ans, answer)
         if not is_correct:
             print(f"Let's try again, {user_name}!")
             break
-        condition_data = module.get_condition_data()
-        correct_ans = module.get_correct_answer(condition_data)
-        print(f"Question: {condition_data}")
-        answer = prompt.string("Your answer: ")
-        is_correct = module.is_user_correct(correct_ans, answer)
     else:
         print(f"Congratulations, {user_name}!")
+    
